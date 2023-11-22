16ca6f3d6b87fd43a909aba9f0713e5811a77c08
onCloud <modimodi@mjc.ac.kr>
2023-11-23 01:20:44 +0900

    JSON commit
    yes

---
+++ src/StatusBars.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.border.EmptyBorder;
+
+public class StatusBars extends JPanel {
+    public StatusBars() {
+        setLayout(new GridLayout(5, 1, 0, 5));
+
+        String[] statusBarData = {"Health", "Stamina", "Thirst"};
+        Color[] statusBarColors = {new Color(150, 0, 0), new Color(150, 150, 0), new Color(0, 0, 150)};
+
+        for (int i = 0; i < statusBarData.length; i++) {
+            JProgressBar statusBar = new JProgressBar();
+            statusBar.setString(statusBarData[i]);
+            statusBar.setBackground(statusBarColors[i]);
+            statusBar.setStringPainted(true);
+            add(statusBar);
+        }
+
+        int rightPadding = 200;
+        setBorder(new EmptyBorder(0, 0, 0, rightPadding));
+    }
+}
