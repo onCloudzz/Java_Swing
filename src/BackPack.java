092ea3e277dd9812c146a6f43a36da3b6987482d
onCloud <modimodi@mjc.ac.kr>
2023-11-29 23:15:24 +0900

    sub commit

--- src/BackPack.java
+++ src/BackPack.java
@@ -1,30 +1,81 @@
 public class BackPack {
-    private  int[]  inventory;
+    private  int[]  weaponinventory;
+    private  int[]  armorinventory;
+    private  int[]  foodinventory;
     private int size;
     private int stack;
 
-    public void setBackPack(int size){
-        inventory = new int[size];
+    public void setWeaponInventory(int size){
+        weaponinventory = new int[size];
         stack = 0;
     }
 
-    public void addItem(int item){
+    public void addWeapon(int item){
         if(stack < size){
-            inventory[stack] = item;
+            weaponinventory[stack] = item;
             stack++;
         }
     }
-    public void removeItem(int item){
+    public void removeWeapon(int item){
         for(int i = 0; i < stack; i++){
-            if(inventory[i] == item){
-                inventory[i] = 0;
+            if(weaponinventory[i] == item){
+                weaponinventory[i] = 0;
                 stack--;
                 break;
             }
         }
     }
-    public int[] getInventory(){
-        return inventory;
+    public void setArmorInventory(int size){
+        armorinventory = new int[size];
+        stack = 0;
+    }
+
+    public void addArmor(int item){
+        if(stack < size){
+            armorinventory[stack] = item;
+            stack++;
+        }
+    }
+
+    public void removeArmor(int item){
+        for(int i = 0; i < stack; i++){
+            if(armorinventory[i] == item){
+                armorinventory[i] = 0;
+                stack--;
+                break;
+            }
+        }
+    }
+
+    public void setFoodInventory(int size){
+        foodinventory = new int[size];
+        stack = 0;
+    }
+
+    public void addFood(int item){
+        if(stack < size){
+            foodinventory[stack] = item;
+            stack++;
+        }
+    }
+
+    public void removeFood(int item){
+        for(int i = 0; i < stack; i++){
+            if(foodinventory[i] == item){
+                foodinventory[i] = 0;
+                stack--;
+                break;
+            }
+        }
+    }
+    public int[] getWeaponInventory(){
+        return weaponinventory;
+    }
+    public int[] getArmorInventory(){
+        return armorinventory;
+    }
+    public int[] getFoodInventory(){
+        return foodinventory;
     }
     public int getStack(){
         return stack;
---
+++ src/PlayerInfo.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class PlayerInfo {
+    //플레이어 체력
+    private int HP;
+    //플레이어 공격력
+    private int ATK;
+    //플레이어 방어력
+    private int DEF;
+    //플레이어 허기 수치
+    private int Hunger;
+    //플레이어 갈증 수치
+    private int Thirst;
+    //플레이어 현재 층
+    private int Floor;
+    //플레이어 현재 방
+    private int Room;
+    //플레이어 현재 무기
+    private int Weapon;
+    //플레이어 현재 갑옷
+    private int Armor;
+    //플레이어
+}
